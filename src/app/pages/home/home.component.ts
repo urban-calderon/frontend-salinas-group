4fe7659a93d69f05f2c82adadf60fb0364406b8d
urban-calderon <urbanocalderon@outlook.com>
2025-06-01 17:46:03 -0600

    feat: add validation to the text field

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'home-page',
@@ -7,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  public maxLength: number = 15;
+
+  nameControl = new FormControl('', [
+    Validators.required,
+    Validators.maxLength(this.maxLength),
+    Validators.pattern(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ ]*$/)
+  ]);
+
+  get charCount(): number {
+    return this.nameControl.value?.length || 0;
+  }
+
+  emitName(): void {
+    if (this.nameControl.valid) {
+      console.log('Nombre válido:', this.nameControl.value);
+    } else {
+      console.log('no valido')
+      this.nameControl.markAsTouched();
+    }
+  }
+
+  getErrorMessage(): string {
+    if (this.nameControl.hasError('required')) {
+      return 'Este campo es obligatorio';
+    }
+    if (this.nameControl.hasError('maxlength')) {
+      return `Máximo ${this.maxLength} caracteres`;
+    }
+    if (this.nameControl.hasError('pattern')) {
+      return 'Solo se permiten carácteres alfanuméricos';
+    }
+    return '';
+  }
 
 }
