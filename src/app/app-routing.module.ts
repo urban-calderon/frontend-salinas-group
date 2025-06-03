c964415dfac383725650e098bbe76c20beb988e7
urban-calderon <urbanocalderon@outlook.com>
2025-06-03 01:41:32 -0600

    feat: add routing configuration for the application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomePage } from './pages/home/home.component';
+import { EncryptPage } from './pages/encrypt/encrypt.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomePage
+  },
+  {
+    path: 'encrypt',
+    component: EncryptPage
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
